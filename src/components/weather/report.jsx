35da76c5355f983acd1d18fbe8c592d50df0bd0b
Rob Welan <robwelan@yahoo.com.au>
2018-07-22 12:08:50 +1000

    Update WeatherReport
    - show 'Weather Report' only when no data is available
    - include placeholder code in preparation of future needs

--- src/components/weather/report.jsx
+++ src/components/weather/report.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
+import {
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Responsive,
+  Statistic,
+} from 'semantic-ui-react';
 // External Libraries
 import locale2 from 'locale2';
+// Sibling Components
+// import ControlUnits from './control-units';
+// import ForecastRowDayNames from './forecast-row-day-names';
+// import ForecastRowIcons from './forecast-row-icons';
+// import ForecastRowRisesAndSets from './forecast-row-rises-and-sets';
+// import ForecastRowTemperatures from './forecast-row-temperatures';
+// import GridResponsiveLocationData from './grid-responsive-location-data';
+// import RowMiscellaneous from './row-miscellaneous';
+// import RowSunMoon from './row-sun-moon';
+// import RowWind from './row-wind';
+// Custom Code
+import {
+  getCurrentMiscellaneous,
+  getCurrentWind,
+  getForecastCurrentDay,
+  getForecastRowDayNames,
+  getForecastRowIcons,
+  getForecastRowRisesAndSets,
+  getForecastRowTemperatures,
+} from './data-snippets';
+import { getDateStringFromEpoch, getDayStringFromEpoch, isObjectEmpty } from '../../engine/tools';
+import { currentUnitType, forecastUnitType } from './utilities';
+import { getkApixuColor, getApixuIconName } from './icon-map';
+// Custom Styles
+import 'weather-icons-tmp/css/weather-icons.min.css';
+import 'weather-icons-tmp/css/weather-icons-wind.min.css';
+import './index.css';
 
 class WeatherReport extends React.Component {
   constructor(props) {
@@ -22,11 +57,22 @@ class WeatherReport extends React.Component {
 
   render() {
     const { activeUnit, defaultUnit, isBrowserMetric } = this.state;
+    const isEmpty = isObjectEmpty(this.props);
 
     return (
-      <h1>
-        Hello World!
-      </h1>
+      <div>
+        {isEmpty && (
+          <Grid divided="vertically">
+            <Grid.Row columns={1}>
+              <Grid.Column textAlign="center">
+                <h2>
+                  Weather Report
+                </h2>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        )}
+      </div>
     );
   }
 }
